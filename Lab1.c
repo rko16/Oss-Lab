b44d3ebfd9f869e47e5f7a16ad06be69ac18cd6d
Ram Krishna Srivastava <68686108+rko16@users.noreply.github.com>
2021-09-01 15:11:38 +0530

    Add files via upload

---
+++ Lab1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Input the two integers a and b\n");
+    scanf("%d %d", &a, &b);
+    printf("Xor of a and b %d\n", a^b);
+    return 0;
+}
